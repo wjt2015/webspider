c248ca575ec93348cc87686608ade6edca6256ed
wjt_weibo <15236286736@163.com>
2020-05-04 18:32:23 +0800

    update

--- src/main/java/com/wjt/config/DBConfig.java
+++ src/main/java/com/wjt/config/DBConfig.java
@@ -1,9 +1,17 @@
 package com.wjt.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
 /**
  * @Time 2020/4/22/1:31
  * @Author jintao.wang
  * @Description
  */
+@Configuration
+@ImportResource()
 public class DBConfig {
+
+
+
 }
