244c0af148a4489c00db85f0b30e0b5e97961adc
jintao9 <15236286736@163.com>
2020-10-13 12:09:40 +0800

    添加myJedis

--- src/main/java/com/wjt/model/JunjinArticleEntity.java
+++ src/main/java/com/wjt/model/JunjinArticleEntity.java
@@ -2,6 +2,21 @@ package com.wjt.model;
 
 import java.util.Date;
 
+/**
+ create table if not exists article(
+ id bigint not null auto_increment comment '自增主键id',
+ create_time timestamp default current_timestamp comment '记录的创建时间',
+ update_time timestamp default current_timestamp on update current_timestamp comment '记录的更新时间',
+ create_user varchar(256) not null default '' comment '记录创作者名字',
+ update_user varchar(256) not null default '' comment '记录更新者名字',
+ title varchar(256) not null default '' comment '文章标题',
+ url varchar(512) not null default '' comment '文章链接',
+ summary varchar(512) not null default '' comment '文章概要',
+ primary key(id)
+ )default character set='utf8' comment='技术文章数据表' engine=innodb;
+
+
+ */
 public class JunjinArticleEntity {
     public Long id;
 
