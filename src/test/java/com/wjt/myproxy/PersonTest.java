69ad630bc7668b7cd28b8d513f16ca80547b1c76
王锦涛 <jintao9@staff.weibo.com>
2020-11-07 22:23:26 +0800

    spring源码学习笔记

--- src/test/java/com/wjt/myproxy/PersonTest.java
+++ src/test/java/com/wjt/myproxy/PersonTest.java
@@ -4,6 +4,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import sun.misc.ProxyGenerator;
 
+import javax.tools.JavaCompiler;
+import javax.tools.JavaFileObject;
+import javax.tools.StandardJavaFileManager;
+import javax.tools.ToolProvider;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -35,6 +39,30 @@ public class PersonTest {
 
         String fileName="/Users/jintao9/linux2014/wjt_projs/webspider/docs/logs/proxy.txt";
         Files.write(Paths.get(fileName),bytes);
+
+    }
+
+
+    @Test
+    public void javaCompiler(){
+
+        String srcFileNameA,srcFileNameB;
+        srcFileNameA="/Users/jintao9/linux2014/wjt_projs/webspider/src/test/java/com/wjt/config/AppConfigTest.java";
+        srcFileNameB="/Users/jintao9/linux2014/wjt_projs/webspider/src/main/java/com/wjt/service/impl/BloomFilterImpl.java";
+
+        JavaCompiler javaCompiler = ToolProvider.getSystemJavaCompiler();
+
+        try (StandardJavaFileManager fileManager = javaCompiler.getStandardFileManager(null, null, null)){
+            Iterable<? extends JavaFileObject> javaFileObjects = fileManager.getJavaFileObjects(srcFileNameA, srcFileNameB);
+
+            Boolean call = javaCompiler.getTask(null, fileManager, null, null, null, javaFileObjects).call();
+
+            log.info("call={};javaFileObjects={};",call,javaFileObjects);
+        }catch (Exception e){
+            log.error("java compile error!",e);
+        }
+
+
 
     }
 
